544361a471babbfafb9ff2702a9cc5e5b46cc5d5
EricLanteigne <eric.lanteigne@carleton.ca>
2016-02-27 01:14:36 -0500

    New changes

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,25 @@
 function switchToLogin(){
 	document.getElementById("homePage").style.display = "None";
-	document.getElementById("log_in_form").style.display = "Block";
+	document.getElementById("logInForm").style.display = "Block";
+	document.getElementById("signUpForm").style.display = "None";
 }
 
 function switchToSignUp(){
 	document.getElementById("homePage").style.display = "None";
-	document.getElementById("sign_up_form").style.display = "Block";
+	document.getElementById("signUpForm").style.display = "Block";
+	document.getElementById("logInForm").style.display = "None";
+}
+
+function switchToHome(){
+	document.getElementById("homePage").style.display = "Block";
+	document.getElementById("signUpForm").style.display = "None";
+	document.getElementById("logInForm").style.display = "None";
+}
+
+function logIn(){
+	alert("logIn");
+}
+
+function signUp(){
+	alert("signUp");
 }
