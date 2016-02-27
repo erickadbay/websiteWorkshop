c98edccd7988132e655c03ad3e6cbd07177944b9
EricLanteigne <eric.lanteigne@carleton.ca>
2016-02-27 03:00:21 -0500

    changes

---
+++ connect.php
@@ -0,0 +1,14 @@
+
+<?php
+
+$mysql_host = "localhost";
+$mysql_user = "root";
+$mysql_pass = "";
+
+if (mysql_connect($mysql_host,$mysql_user,$mysql_pass) && mysql_select_db("cunote")){
+
+}else{
+	echo mysql_error();
+}
+
+?>
