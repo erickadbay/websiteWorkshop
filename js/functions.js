a0c7a61413394195e0babe100bf8624e9428772e
erickadbay <erickadbay@gmail.com>
2016-02-27 01:52:16 -0500

    added validation for login form and signup form

--- js/functions.js
+++ js/functions.js
@@ -17,9 +17,35 @@ function switchToHome(){
 }
 
 function logIn(){
-	alert("logIn");
+	if (!validateField("username")){
+		alert("Please enter a username");
+	}else if(!validateField("pwd")){
+		alert("Please enter a password");
+	}
 }
 
 function signUp(){
-	alert("signUp");
+	if (!validateField("username")){
+		alert("Please enter a username");
+	}else if(!validateField("pwd")){
+		alert("Please enter a password");
+	}else if(!validateField("confirmPwd")){
+		alert("Please confirm your password");
+	}else if(!validateField("firstName")){
+		alert("Please enter your first name");
+	}else if(!validateField("lastName")){
+		alert("Please enter your last name");
+	}else if(!validateField("email")){
+		alert("Please enter an email address");
+	}
+}
+
+function validateField(id){
+	var field = document.getElementById(""+id);
+
+	if(field.value== ""){
+		return false;
+	}else{
+		return true;
+	}
 }
